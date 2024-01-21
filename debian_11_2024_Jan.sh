c9dc63fcc2ba3a2120bdce75f235f35ca99c8b3d
mendo1024 <34595518+mendo1024@users.noreply.github.com>
2024-01-21 17:57:53 +0100

    Update debian_11_2024_Jan.sh

--- debian_11_2024_Jan.sh
+++ debian_11_2024_Jan.sh
@@ -361,7 +361,92 @@ WantedBy=multi-user.target
 EOF
 
 sudo cp -v $BUILD_DIR/ospd-openvas.service /etc/systemd/system/
+## notus-scanner.service
+cat << EOF > $BUILD_DIR/notus-scanner.service
+[Unit]
+Description=Notus Scanner
+Documentation=https://github.com/greenbone/notus-scanner
+After=mosquitto.service
+Wants=mosquitto.service
+ConditionKernelCommandLine=!recovery
 
+[Service]
+Type=exec
+User=gvm
+RuntimeDirectory=notus-scanner
+RuntimeDirectoryMode=2775
+PIDFile=/run/notus-scanner/notus-scanner.pid
+ExecStart=/usr/local/bin/notus-scanner --foreground --products-directory /var/lib/notus/products --log-file /var/log/gvm/notus-scanner.log
+SuccessExitStatus=SIGKILL
+Restart=always
+RestartSec=60
+
+[Install]
+WantedBy=multi-user.target
+EOF
+sudo cp -v $BUILD_DIR/notus-scanner.service /etc/systemd/system/
+## gvmd.service
+cat << EOF > $BUILD_DIR/gvmd.service
+[Unit]
+Description=Greenbone Vulnerability Manager daemon (gvmd)
+After=network.target networking.service postgresql.service ospd-openvas.service
+Wants=postgresql.service ospd-openvas.service
+Documentation=man:gvmd(8)
+ConditionKernelCommandLine=!recovery
+
+[Service]
+Type=exec
+User=gvm
+Group=gvm
+PIDFile=/run/gvmd/gvmd.pid
+RuntimeDirectory=gvmd
+RuntimeDirectoryMode=2775
+ExecStart=/usr/local/sbin/gvmd --foreground --osp-vt-update=/run/ospd/ospd-openvas.sock --listen-group=gvm
+Restart=always
+TimeoutStopSec=10
+
+[Install]
+WantedBy=multi-user.target
+EOF
+sudo cp -v $BUILD_DIR/gvmd.service /etc/systemd/system/
+## gsad.service
+cat << EOF > $BUILD_DIR/gsad.service
+[Unit]
+Description=Greenbone Security Assistant daemon (gsad)
+Documentation=man:gsad(8) https://www.greenbone.net
+After=network.target gvmd.service
+Wants=gvmd.service
+
+[Service]
+Type=exec
+User=gvm
+Group=gvm
+RuntimeDirectory=gsad
+RuntimeDirectoryMode=2775
+PIDFile=/run/gsad/gsad.pid
+ExecStart=/usr/local/sbin/gsad --foreground --listen=127.0.0.1 --port=9392 --http-only
+Restart=always
+TimeoutStopSec=10
+
+[Install]
+WantedBy=multi-user.target
+Alias=greenbone-security-assistant.service
+EOF
+sudo cp -v $BUILD_DIR/gsad.service /etc/systemd/system/
+## systemctl reload
+sudo systemctl daemon-reload
+## Downloading the Data
+sudo /usr/local/bin/greenbone-feed-sync
+# Starting the Greenbone Community Edition Services
+sudo systemctl start notus-scanner
+sudo systemctl start ospd-openvas
+sudo systemctl start gvmd
+sudo systemctl start gsad
+sudo systemctl enable notus-scanner
+sudo systemctl enable ospd-openvas
+sudo systemctl enable gvmd
+sudo systemctl enable gsad
+xdg-open "http://127.0.0.1:9392" 2>/dev/null >/dev/null &
 
 
 
