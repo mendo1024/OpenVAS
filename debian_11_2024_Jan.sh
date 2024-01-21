c68faf2bb79438830c716ca52bd1ec521e2f91d8
mendo1024 <34595518+mendo1024@users.noreply.github.com>
2024-01-21 18:21:31 +0100

    Update debian_11_2024_Jan.sh

--- debian_11_2024_Jan.sh
+++ debian_11_2024_Jan.sh
@@ -21,6 +21,10 @@ export INSTALL_DIR=$HOME/install
 mkdir -p $INSTALL_DIR
 # Installing Common Build Dependencies
 sudo apt update
+sudo apt install dirmngr ca-certificates software-properties-common apt-transport-https lsb-release curl -y
+curl -fsSl https://www.postgresql.org/media/keys/ACCC4CF8.asc | gpg --dearmor | sudo tee /usr/share/keyrings/postgresql.gpg > /dev/null
+echo deb [arch=amd64,arm64,ppc64el signed-by=/usr/share/keyrings/postgresql.gpg] http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main | sudo tee /etc/apt/sources.list.d/postgresql.list
+sudo apt update
 sudo apt install --no-install-recommends --assume-yes \
   build-essential \
   curl \
